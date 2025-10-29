5978195c2769eec7b41fb18c2004a6a36dc209db
Mayuri Futane <futanemayuri0@gmail.com>
2025-10-29 14:19:41 +0530

    Update students.js

--- Backend/src/routes/students.js
+++ Backend/src/routes/students.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./src/db/attendance.db');
 
+router.get('/', (req, res) => {
+  db.all('SELECT * FROM students', [], (err, rows) => {
+    if(err) res.status(500).json(err);
+    else res.json(rows);
+  });
+});
+
+module.exports = router;
